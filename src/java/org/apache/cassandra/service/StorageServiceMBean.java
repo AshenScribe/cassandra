d6a8e1cdf19db45d2468b54ac537a1dc41b235ed
omniCoder77 <rishabhsaraswat17@gmail.com>
2026-03-05 02:16:07 +0530

    updated commit to target only preparable statements

--- src/java/org/apache/cassandra/service/StorageServiceMBean.java
+++ src/java/org/apache/cassandra/service/StorageServiceMBean.java
@@ -1100,12 +1100,12 @@ public interface StorageServiceMBean extends NotificationEmitter
     /**
      * Returns whether use of misprepared statements is enabled
      */
-    public boolean getMispreparedStatementsEnabled();
+    public boolean getPreparedStatementsRequireParametersEnabled();
 
     /**
      * Sets whether use of misprepared statements is enabled
      */
-    public void setMispreparedStatementsEnabled(boolean enabled);
+    public void setPreparedStatementsRequireParametersEnabled(boolean enabled);
 
     /** Returns the threshold for skipping the column index when caching partition info **/
     public int getColumnIndexCacheSizeInKiB();
