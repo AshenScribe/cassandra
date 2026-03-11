99d592bc1633db37d28d293f19334367e2ee2dd9
omniCoder77 <rishabhsaraswat17@gmail.com>
2026-03-11 23:29:34 +0530

    type subsribe to subscribe

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -3886,7 +3886,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
         {
             ChannelGroup channelGroup = CassandraDaemon.instance.nativeTransportService()
                                                                 .getServer()
-                                                                .getChannelsSubsribedToGracefulDisconnect();
+                                                                .getChannelsSubscribedToGracefulDisconnect();
 
             if (channelGroup != null && !channelGroup.isEmpty())
             {
